428e39286811c8b808b4e8566e24a2b05cc32ad2
H Hautakoski <henrik.hautakoski@gmail.com>
2010-06-18 16:32:03 +0200

    lib/prime.c: fixed a bug where 2 was not a prime in is_prime().

--- lib/prime.c
+++ lib/prime.c
@@ -6,6 +6,8 @@ int is_prime(uint32_t p) {
 
     uint32_t i, l;
 
+    if (p == 2)
+        return 1;
     if (p < 2 || (p & 1) == 0)
         return 0;
 
--- lib/prime.h
+++ lib/prime.h
@@ -1,6 +1,5 @@
 
 #ifndef __PRIME_H
-
 #define __PRIME_H
 
 #include <stdint.h>

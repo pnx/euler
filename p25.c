296a8b84c347f86e6399f6b0bcba6a17170204a5
H Hautakoski <henrik@fiktivkod.org>
2010-07-13 09:43:26 +0200

    problem 25

---
+++ p25.c
@@ -0,0 +1,24 @@
+
+/*
+ * http://projecteuler.net
+ * 
+ * Projecteuler - Problem 25
+ * -----------------------
+ * 2010-07-13 Henrik Hautakoski
+ *
+ * Alot of math, binet's formula
+ */
+
+#include <math.h>
+#include <stdio.h>
+
+#define LOG10PHI 0.20898764024997876221 /* log10((1+sqrt(5))/2) */
+#define MAGIC    0.65051499783199062676 /* |(log10(5) / 2) - 1| */
+
+#define fibolen(n) ((int) ceil(((n) - MAGIC) / LOG10PHI))
+
+int main() {
+    
+    printf("%i\n", fibolen(1000));
+    return 0;
+}
